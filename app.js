bd777bef4362ca3f9145a680d04519133ba4b04b
Turkanyasar <turkanaliyevayasar@gmail.com>
2026-05-14 13:16:44 +0000

    media

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 "use strict";
 $(document).ready(function () {
 
-});
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let a=$('input').val();
+        let num=Number(a);
+
+        if (a.trim() !=="" &&! isNaN(num)) {
+            for (let i=0; i<num; i++) {
+                $('.col-md-12').append(`<div class="todo-item">${num}</div>`);
+            }
+        }
+        else if (a.trim() !=="") {
+            $('.col-md-12').append(`<div class="todo-item">${a}</div>`);
+        }
+        $('input').val('');
+    });
+    });
+
+
+
+
+
